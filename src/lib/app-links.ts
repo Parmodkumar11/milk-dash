c16e8322d8e2532a97fecf2ee9b9ff5e58656172
Parmod <parmodcuilsoft@gmail.com>
2026-09-14 23:16:16 +0530

    Update WhatsApp number

--- src/lib/app-links.ts
+++ src/lib/app-links.ts
@@ -1,5 +1,5 @@
 export const ANDROID_APP_URL =
-  'https://drive.google.com/file/d/1WitiFfB7WXMryrjXqvPZEXCXoPOKNfyt/view?usp=sharing';
+  'https://drive.google.com/file/d/1DC0FngG5QaQDCeZ3zjcXvPdIF0EGe9qZ/view?usp=sharing';
 
 export function openAndroidApp() {
   window.location.assign(ANDROID_APP_URL);
